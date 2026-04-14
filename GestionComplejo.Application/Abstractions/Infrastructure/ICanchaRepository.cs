298e66713bd65bf8988203c076126e8bac61b902
Agustin Baez <agustinramirobaez@hotmail.com>
2026-04-14 20:32:45 -0300

    add inf layer

---
+++ GestionComplejo.Application/Abstractions/Infrastructure/ICanchaRepository.cs
@@ -0,0 +1,9 @@
+﻿using GestionComplejo.Domain.Entities;
+
+namespace GestionComplejo.Application.Abstractions.Infrastructure
+{
+    public interface ICanchaRepository : IBaseRepository<Cancha>
+    {
+
+    }
+}
---
+++ GestionComplejo.Infrastructure/Persistance/Repository/CanchaRepository.cs
@@ -0,0 +1,15 @@
+﻿using GestionComplejo.Application.Abstractions.Infrastructure;
+using GestionComplejo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionComplejo.Infrastructure.Persistance.Repository
+{
+    public class CanchaRepository : BaseRepository<Cancha>, ICanchaRepository
+    {
+        public CanchaRepository(GestionComplejoDbContext context) : base(context)
+        {
+        }
+    }
+}
